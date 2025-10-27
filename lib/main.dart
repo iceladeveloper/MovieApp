97937900cd92f38ccf2ba9f3a96729adaa2a9c3f
ZAIRA ICELA SILVA SANCHEZ <zaira.silva@udgvirtual.udg.mx>
2025-10-26 22:35:17 -0600

    agregar inicio de proyecto

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:movie_widgets/src/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      initialRoute: 'Inicio',       
+      routes: {
+          'Inicio': (BuildContext context) => HomePage(),
+      }
+    );
+  }
+}
